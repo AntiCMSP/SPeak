13b92e4517ac2a47ed4a4a29262fc2b7140cb9ee
Junior Schueller <50379079+JuniorSchueller@users.noreply.github.com>
2026-07-02 20:17:20 -0300

    feat: add speak loader userscript

---
+++ loader.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         AntiCMSP SPeak
+// @namespace    https://anticmsp.foo.ng/
+// @version      2026-01-07
+// @description  Cansado de ter que pensar nas respostas das lições do SPeak? AntiCMSP SPeak chegou.
+// @author       AntiCMSP Scripts
+// @match        *://learn.better.efekta.com/*
+// @match        *://lesson-player.study.better.efekta.com/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM.cookie
+// @grant        GM_cookie
+// @require      https://github.com/AntiCMSP/SPeak/raw/refs/heads/main/speak.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('[AntiCMSP Scripts] SPeak Loader loaded!'); 
+})();
